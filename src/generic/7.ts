68c8fec4daa35005ff37f48bb9afca8e57fd21c9
AlexDvor <alexmatvichuk@gmail.com>
2025-11-09 10:41:03 +0100

    add task 2

--- src/generic/7.ts
+++ src/generic/7.ts
@@ -4,16 +4,21 @@
 */
 
 export enum UserRole {
-  admin = 'admin',
-  editor = 'editor',
-  guest = 'guest',
+	admin = 'admin',
+	editor = 'editor',
+	guest = 'guest',
 }
 
-// Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
-  admin: 'Admin User',
-  editor: 'Editor User',
-  guest: 'Guest User',
-};
+// // Замініть наступний код на версію за допомогою Record
+// const RoleDescription = {
+//   admin: 'Admin User',
+//   editor: 'Editor User',
+//   guest: 'Guest User',
+// };
 
-export {};
+export const RoleDescription: Record<UserRole, string> = {
+	[UserRole.admin]: 'Admin User',
+	[UserRole.editor]: 'Editor User',
+	[UserRole.guest]: 'Guest User',
+}
+export {}
